e37da30332d11765751c17451069a48fb7bf8483
Ataotao <tzvuf@163.com>
2016-12-24 11:41:26 +0800

    $q

---
+++ $q玩转 AngularJS 的 Promise/一个异步模式的实例/js/controller.js
@@ -0,0 +1,18 @@
+angular.module("MyController", [])
+    .controller("IndexController", ["$scope", "githubService", "$timeout", function($scope, githubService, $timeout) {
+        $scope.name = "dreamapple";
+        $scope.show = true;
+        githubService.getPullRequests()
+            .then(function(result) {
+                $scope.data = result;
+            }, function(error) {
+                $scope.data = "error!";
+            }, function(progress) {
+                $scope.progress = progress;
+                $scope.show = false;
+            });
+
+    }]);
+
+
+
